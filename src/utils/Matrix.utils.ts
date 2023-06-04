f2b07f69dd0c2d008b2361dc1911c0be7717f050
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 04:00:40 -0300

    Add Matrix4x4 type and MatrixUtils

---
+++ src/utils/Matrix.utils.ts
@@ -0,0 +1,14 @@
+import { Matrix4x4 } from "../core/Matrix4x4";
+
+export class MatrixUtils {
+
+    public static newMatrix4x4(): Matrix4x4 {
+
+        return [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+    }
+}
