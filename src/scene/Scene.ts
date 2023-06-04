0a939702f39acd99962579ae7bd38f266d3c8549
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 04:14:05 -0300

    Add meshes to scene

--- src/scene/Scene.ts
+++ src/scene/Scene.ts
@@ -1,9 +1,13 @@
+import { Mesh } from "../core/Mesh";
 import { SceneConfig } from "./SceneConfig";
 
 export class Scene {
 
+    public meshes: Array<Mesh>;
+
     constructor(config: SceneConfig = new SceneConfig()) {
 
+        this.meshes = Array<Mesh>();
     }
 
     public preload(): void {
