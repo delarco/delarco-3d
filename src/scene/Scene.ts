5d488762b0c0e932fe893ecb6dfae0aa15aac933
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 17:49:48 -0300

    Enable ambientLight property to game class

--- src/scene/Scene.ts
+++ src/scene/Scene.ts
@@ -3,10 +3,14 @@ import { SceneConfig } from "./SceneConfig";
 
 export class Scene {
 
+    private config: SceneConfig;
     public meshes: Array<Mesh>;
+
+    public get ambientLight() { return this.config.ambientLight; }
 
     constructor(config: SceneConfig = new SceneConfig()) {
 
+        this.config = config;
         this.meshes = Array<Mesh>();
     }
 
