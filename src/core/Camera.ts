b818fa850e30ca104e251c2b9628e6ab42b1aca5
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 17:25:07 -0300

    Add position to the camera

--- src/core/Camera.ts
+++ src/core/Camera.ts
@@ -1,3 +1,5 @@
+import { Vec3D } from "./Vect3D";
+
 export class Camera {
 
     public fovRad: number;
@@ -7,6 +9,7 @@ export class Camera {
         public aspectRatio: number = 0.75,
         public near: number = 0.1,
         public far: number = 1000.0,
+        public position = new Vec3D(),
     ) {
 
         this.fovRad = 1.0 / Math.tan(fov * 0.5 / 180.0 * Math.PI);
