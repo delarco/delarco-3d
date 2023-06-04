4403967ecaa2e0b035aea3227bbf3d9c62c113f9
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 03:47:04 -0300

    Update main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './style.css'
 import { Game } from './game/Game';
+import { TestScene } from './test-scene';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 const game = new Game();
 app.appendChild(game.domElement);
+
+game.scene = new TestScene();
+game.run();
