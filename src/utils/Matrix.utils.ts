7efe7a9057896561b44ec7c612325dafe2e13f21
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 04:03:02 -0300

    Implement createProjectionMatrix method

--- src/utils/Matrix.utils.ts
+++ src/utils/Matrix.utils.ts
@@ -1,3 +1,4 @@
+import { Camera } from "../core/Camera";
 import { Matrix4x4 } from "../core/Matrix4x4";
 
 export class MatrixUtils {
@@ -10,5 +11,19 @@ export class MatrixUtils {
             [0, 0, 0, 0],
             [0, 0, 0, 0],
         ]
+    }
+
+    public static createProjectionMatrix(camera: Camera): Matrix4x4 {
+
+        const projectionMat = MatrixUtils.newMatrix4x4();
+
+        projectionMat[0][0] = camera.aspectRatio * camera.fovRad;
+        projectionMat[1][1] = camera.fovRad;
+        projectionMat[2][2] = camera.far / (camera.far - camera.near);
+        projectionMat[3][2] = (-camera.far * camera.near) / (camera.far - camera.near);
+        projectionMat[2][3] = 1.0;
+        projectionMat[3][3] = 0.0;
+
+        return projectionMat;
     }
 }
