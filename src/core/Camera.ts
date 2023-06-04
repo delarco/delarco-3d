f5a30b07b3c212db6a5f675f609d84725af043c1
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 03:55:18 -0300

    Precalculate fovRad

--- src/core/Camera.ts
+++ src/core/Camera.ts
@@ -1,9 +1,14 @@
 export class Camera {
 
+    public fovRad: number;
+
     constructor(
         public fov: number = 90.0,
         public aspectRatio: number = 0.75,
         public near: number = 0.1,
         public far: number = 1000.0,
-    ) { }
+    ) {
+
+        this.fovRad = 1.0 / Math.tan(fov * 0.5 / 180.0 * Math.PI);
+    }
 }
