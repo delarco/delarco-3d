6ddc5ed977d8abd97f8433470a8e05331a7380f5
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 17:51:02 -0300

    Add vectorLenght method

--- src/utils/Vector.utils.ts
+++ src/utils/Vector.utils.ts
@@ -6,4 +6,9 @@ export class VectorUtils {
 
         return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z ;
     }
+
+    public static vectorLength(v: Vec3D): number {
+
+        return Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    }
 }
