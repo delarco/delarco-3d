e550b96929288f67229aa2ee5aae61d66107b559
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 15:40:45 -0300

    Fix triangle point getters

--- src/core/Triangle.ts
+++ src/core/Triangle.ts
@@ -3,8 +3,8 @@ import { Vec3D } from "./Vect3D";
 export class Triangle {
 
     public get p1() { return this.points[0]; }
-    public get p2() { return this.points[0]; }
-    public get p3() { return this.points[0]; }
+    public get p2() { return this.points[1]; }
+    public get p3() { return this.points[2]; }
 
     constructor(
         public points: Array<Vec3D> = new Array<Vec3D>(
