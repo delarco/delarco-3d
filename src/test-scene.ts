4af46ad411b6e57eedb84e2c61ed930362e399bf
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-04 03:46:45 -0300

    Create a test scene

---
+++ src/test-scene.ts
@@ -0,0 +1,14 @@
+import { Scene } from "./scene/Scene";
+
+export class TestScene extends Scene {
+
+    public preload(): void {
+
+        
+    }
+
+    public update(time: number, deltaTime: number): void {
+
+        
+    }
+}
