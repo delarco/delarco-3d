d315e0b25b8322819038e2c0b26b0bf9105d2020
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-05 01:46:37 -0300

    Update test scene

--- src/test-scene.ts
+++ src/test-scene.ts
@@ -1,33 +1,19 @@
 import { CubeGeometry } from "./geometry/CubeGeometry";
 import { Scene } from "./scene/Scene";
-import { WavefrontObjLoader } from "./utils/WavefrontObjLoader";
 
 export class TestScene extends Scene {
 
     public async preload(): Promise<void> {
-
-        const lamp = await WavefrontObjLoader.loadFile('/models/lamp.obj');
-        const teapot = await WavefrontObjLoader.loadFile('/models/teapot.obj');
-
-        if (teapot) this.meshes.push(teapot);
 
         this.meshes.push(new CubeGeometry(1))
     }
 
     public initialize(): void {
 
-        this.meshes[0].translation.z = 9;
-        this.meshes[0].translation.y = 3;
-        this.meshes[1].translation.z = 9;
-        this.meshes[0].roation.z = 3.14;
+        this.meshes[0].translation.z = 3;
     }
 
     public update(time: number, deltaTime: number): void {
 
-        for (let mesh of this.meshes) {
-
-            mesh.roation.x += 1.0 * deltaTime;
-            mesh.roation.z += 1.0 * deltaTime;
-        }
     }
 }
