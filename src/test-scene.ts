393d03c0485efaca38d5aabbc1acfed30c8cf887
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-07 04:51:56 -0300

    Update test scene

--- src/test-scene.ts
+++ src/test-scene.ts
@@ -1,19 +1,35 @@
-import { CubeGeometry } from "./geometry/CubeGeometry";
+import { Color } from "./core/Color";
 import { Scene } from "./scene/Scene";
+import { TextureUtils } from "./utils/Texture.utils";
+import { WavefrontObjLoader } from "./utils/WavefrontObjLoader";
+
 
 export class TestScene extends Scene {
 
     public async preload(): Promise<void> {
 
-        this.meshes.push(new CubeGeometry(1))
+        await this.addObj('models/Earth_2K.obj', 'textures/Diffuse_2K.png');
+    }
+
+    private async addObj(filename: string, texture?: string): Promise<void> {
+
+        const mesh = await WavefrontObjLoader.loadFile(filename, Color.RED);
+
+        if (mesh){
+
+            if(texture) mesh.texture = await TextureUtils.loadTexture(texture);
+            this.meshes.push(mesh);
+        }
     }
 
     public initialize(): void {
 
-        this.meshes[0].translation.z = 3;
+        this.meshes[0].translation.z = 5;
     }
 
     public update(time: number, deltaTime: number): void {
 
+        this.meshes[0].roation.x += 1 * deltaTime;
+        this.meshes[0].roation.z += 1 * deltaTime;
     }
 }
