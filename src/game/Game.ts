da13090cbbb93e50121621fbe201528d36848ca7
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-07 02:14:43 -0300

    Implement drawTexturedTriangle

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -201,9 +201,185 @@ export class Game {
 
         // TODO: clip other planes
 
-        for (let triangle of this.trianglesToRaster.sort(this.triangleSortFunction)) {
+        for (let t of this.trianglesToRaster.sort(this.triangleSortFunction)) {
 
-            this.renderer.drawTriangleWireFrame(triangle);
+            this.drawTexturedTriangle(
+                t.p1.x, t.p1.y, t.t1.u, t.t1.v, t.t1.w,
+                t.p2.x, t.p2.y, t.t2.u, t.t2.v, t.t2.w,
+                t.p3.x, t.p3.y, t.t3.u, t.t3.v, t.t3.w,
+                this.texture
+            );
+
+            this.renderer.drawTriangleWireFrame(t, Color.BLACK);
+        }
+    }
+
+    private drawTexturedTriangle(
+        x1: number, y1: number, u1: number, v1: number, w1: number,
+        x2: number, y2: number, u2: number, v2: number, w2: number,
+        x3: number, y3: number, u3: number, v3: number, w3: number,
+        tex: Texture): void {
+
+            x1 = Math.floor(x1);
+            x2 = Math.floor(x2);
+            x3 = Math.floor(x3);
+            y1 = Math.floor(y1);
+            y2 = Math.floor(y2);
+            y3 = Math.floor(y3);
+
+        if (y2 < y1) {
+            [y1, y2] = [y2, y1];
+            [x1, x2] = [x2, x1];
+            [u1, u2] = [u2, u1];
+            [v1, v2] = [v2, v1];
+            [w1, w2] = [w2, w1];
+        }
+
+        if (y3 < y1) {
+            [y1, y3] = [y3, y1];
+            [x1, x3] = [x3, x1];
+            [u1, u3] = [u3, u1];
+            [v1, v3] = [v3, v1];
+            [w1, w3] = [w3, w1];
+        }
+
+        if (y3 < y2) {
+            [y3, y2] = [y2, y3];
+            [x3, x2] = [x2, x3];
+            [u3, u2] = [u2, u3];
+            [v3, v2] = [v2, v3];
+            [w3, w2] = [w2, w3];
+        }
+
+        let dy1 = y2 - y1;
+        let dx1 = x2 - x1;
+        let dv1 = v2 - v1;
+        let du1 = u2 - u1;
+        let dw1 = w2 - w1;
+
+        let dy2 = y3 - y1;
+        let dx2 = x3 - x1;
+        let dv2 = v3 - v1;
+        let du2 = u3 - u1;
+        let dw2 = w3 - w1;
+
+        let tex_u = 0, tex_v = 0, tex_w = 0;
+
+        let dax_step = 0, dbx_step = 0,
+            du1_step = 0, dv1_step = 0,
+            du2_step = 0, dv2_step = 0,
+            dw1_step = 0, dw2_step = 0;
+
+        if (dy1) dax_step = dx1 / Math.abs(dy1);
+        if (dy2) dbx_step = dx2 / Math.abs(dy2);
+        if (dy1) du1_step = du1 / Math.abs(dy1);
+        if (dy1) dv1_step = dv1 / Math.abs(dy1);
+        if (dy1) dw1_step = dw1 / Math.abs(dy1);
+        if (dy2) du2_step = du2 / Math.abs(dy2);
+        if (dy2) dv2_step = dv2 / Math.abs(dy2);
+        if (dy2) dw2_step = dw2 / Math.abs(dy2);
+
+        if (dy1) {
+            for (let i = y1; i <= y2; i++) {
+
+                let ax = Math.floor(x1 + (i - y1) * dax_step);
+                let bx = Math.floor(x1 + (i - y1) * dbx_step);
+
+                let tex_su = u1 + (i - y1) * du1_step;
+                let tex_sv = v1 + (i - y1) * dv1_step;
+                let tex_sw = w1 + (i - y1) * dw1_step;
+
+                let tex_eu = u1 + (i - y1) * du2_step;
+                let tex_ev = v1 + (i - y1) * dv2_step;
+                let tex_ew = w1 + (i - y1) * dw2_step;
+
+                if (ax > bx) {
+                    [ax, bx] = [bx, ax];
+                    [tex_su, tex_eu] = [tex_eu, tex_su];
+                    [tex_sv, tex_ev] = [tex_ev, tex_sv];
+                    [tex_sw, tex_ew] = [tex_ew, tex_sw];
+                }
+
+                tex_u = tex_su;
+                tex_v = tex_sv;
+                tex_w = tex_sw;
+
+                let tstep = 1.0 / ((bx - ax));
+                let t = 0.0;
+
+                for (let j = ax; j < bx; j++) {
+                    tex_u = (1.0 - t) * tex_su + t * tex_eu;
+                    tex_v = (1.0 - t) * tex_sv + t * tex_ev;
+                    tex_w = (1.0 - t) * tex_sw + t * tex_ew;
+
+                    if (tex_w > this.depthBuffer[i * this.config.resolution.width + j]) {
+
+                        const color = tex.sampleColor(tex_u / tex_w, tex_v / tex_w);
+                        this.renderer.drawPixel(j, i, color);
+                        this.depthBuffer[i * this.config.resolution.width + j] = tex_w;
+                    }
+                    t += tstep;
+                }
+
+            }
+        }
+
+        dy1 = y3 - y2;
+        dx1 = x3 - x2;
+        dv1 = v3 - v2;
+        du1 = u3 - u2;
+        dw1 = w3 - w2;
+
+        if (dy1) dax_step = dx1 / Math.abs(dy1);
+        if (dy2) dbx_step = dx2 / Math.abs(dy2);
+
+        du1_step = 0, dv1_step = 0;
+        if (dy1) du1_step = du1 / Math.abs(dy1);
+        if (dy1) dv1_step = dv1 / Math.abs(dy1);
+        if (dy1) dw1_step = dw1 / Math.abs(dy1);
+
+        if (dy1) {
+            for (let i = y2; i <= y3; i++) {
+
+                let ax = Math.floor(x2 + (i - y2) * dax_step);
+                let bx = Math.floor(x1 + (i - y1) * dbx_step);
+
+                let tex_su = u2 + (i - y2) * du1_step;
+                let tex_sv = v2 + (i - y2) * dv1_step;
+                let tex_sw = w2 + (i - y2) * dw1_step;
+
+                let tex_eu = u1 + (i - y1) * du2_step;
+                let tex_ev = v1 + (i - y1) * dv2_step;
+                let tex_ew = w1 + (i - y1) * dw2_step;
+
+                if (ax > bx) {
+                    [ax, bx] = [bx, ax];
+                    [tex_su, tex_eu] = [tex_eu, tex_su];
+                    [tex_sv, tex_ev] = [tex_ev, tex_sv];
+                    [tex_sw, tex_ew] = [tex_ew, tex_sw];
+                }
+
+                tex_u = tex_su;
+                tex_v = tex_sv;
+                tex_w = tex_sw;
+
+                let tstep = 1.0 / (bx - ax);
+                let t = 0.0;
+
+                for (let j = ax; j < bx; j++) {
+                    tex_u = (1.0 - t) * tex_su + t * tex_eu;
+                    tex_v = (1.0 - t) * tex_sv + t * tex_ev;
+                    tex_w = (1.0 - t) * tex_sw + t * tex_ew;
+
+                    if (tex_w > this.depthBuffer[i * this.config.resolution.width + j]) {
+ 
+                        const color = tex.sampleColor(tex_u / tex_w, tex_v / tex_w);
+                        this.renderer.drawPixel(j, i, color);
+                        this.depthBuffer[i * this.config.resolution.width + j] = tex_w;
+                    }
+                    t += tstep;
+                }
+            }
         }
     }
 
