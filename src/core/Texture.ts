7d0fdc1ee25f175f10add895f10e1a6fd2644d0c
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-07 02:10:44 -0300

    Implement sampleColor

--- src/core/Texture.ts
+++ src/core/Texture.ts
@@ -19,4 +19,17 @@ export class Texture {
 
         return this.data[y * this.width + x];
     }
+
+    public sampleColor(x: number, y: number): Color {
+
+        let sx = Math.floor(x * this.width);
+        let sy = Math.floor(y * this.height - 1.0);
+
+        if (sx < 0) sx = 0;
+        if (sx >= this.width) sx = this.width - 1;
+        if (sy < 0) sy = 0;
+        if (sy >= this.height) sy = this.height - 1;
+
+        return this.getPixelColor(sx, sy);
+    }
 }
