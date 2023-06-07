9fdced1584bd284750d527fe23097f3bb0a3ff70
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-06 22:17:37 -0300

    Add 2D vector and triangle texture coords

--- src/core/Triangle.ts
+++ src/core/Triangle.ts
@@ -1,4 +1,5 @@
 import { Color } from "./Color";
+import { Vec2D } from "./Vect2D";
 import { Vec3D } from "./Vect3D";
 
 export class Triangle {
@@ -10,6 +11,9 @@ export class Triangle {
     constructor(
         public points: Array<Vec3D> = new Array<Vec3D>(
             new Vec3D(), new Vec3D(), new Vec3D()
+        ),
+        public tex: Array<Vec2D> = new Array<Vec2D>(
+            new Vec2D(), new Vec2D(), new Vec2D()
         ),
         public color: Color = Color.RED,
     ) { }
---
+++ src/core/Vect2D.ts
@@ -0,0 +1,8 @@
+export class Vec2D {
+
+    constructor(
+        public u: number = 0,
+        public v: number = 0,
+        public w: number = 1,
+    ) { }
+}
