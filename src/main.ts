aafc8765194f8086ec9fb1569233dc9828a94bb5
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-07 07:54:35 -0300

    Update layout

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import './style.css'
 import { Game } from './game/Game';
 import { TestScene } from './test-scene';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+const scene = document.querySelector<HTMLDivElement>('#scene')!;
 const game = new Game();
-app.appendChild(game.domElement);
+scene.appendChild(game.domElement);
 
 game.scene = new TestScene();
 game.run();
