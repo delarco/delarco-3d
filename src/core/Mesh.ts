87f6dd969c28f708349d9f927f84bbb1403d64b4
Leandro Del Arco <leandrodelarco@outlook.com>
2023-06-07 02:26:16 -0300

    Add texture to mesh

--- src/core/Mesh.ts
+++ src/core/Mesh.ts
@@ -1,3 +1,5 @@
+import { TextureUtils } from "../utils/Texture.utils";
+import { Color } from "./Color";
 import { Triangle } from "./Triangle";
 import { Vec3D } from "./Vect3D";
 
@@ -8,5 +10,6 @@ export class Mesh {
         public triangles = new Array<Triangle>(),
         public roation = new Vec3D(),
         public translation = new Vec3D(),
+        public texture = TextureUtils.make('', 32, 32, Color.RED)
     ) {}
 }
